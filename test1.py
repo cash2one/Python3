658404c3919060426519bc37274f6927e2151624
LucaBrasi89 <andrew.sotnikov.hlam@mail.ru>
2015-12-01 21:20:38 +0200

    Последний коммит перед чем то работоспосбным

--- test1.py
+++ test1.py
@@ -5,12 +5,17 @@
 #        by Luca Brasi, e-mail: andruha.sota@mail.ru
 
 
-import pyautogui, time
+import pyautogui, time, pyxhook, threading
 
 class Base():
     startTime=0
     timerLock=False
-    
+
+    def __init__(self):
+        self.running=True
+        
+
+#   Основная функция
     def main(self):
         lastPos=pyautogui.position()
         
@@ -29,26 +34,53 @@ class Base():
                 print(lastPos,posTime)
                 
             lastPos=currPos
+    
+    
+    def catchKeyPress(self):
+            hm = pyxhook.HookManager()
+            hm.KeyDown = self.OnKeyboardEvent
+            hm.HookKeyboard()
+            hm.start()
+            while self.running:
+                time.sleep(0.1)
+            hm.cancel()
+            
+#    Метод опрелеябщий события нажатия кнопок клавиатуры
+    def OnKeyboardEvent(self,event):
+            print(event.Key, event.Ascii)
+            if event.Ascii == 32:
+                self.running = False
+            elif event.Key == 'Control_L':
+                print(pyautogui.position(), ' LClick')
+                
+
+#   Сей объект класса посвящен таймеру
 
 class Timer(Base):
     
+#    Ставит базовую точку времени с которой начинается отсчет
     def timerStart(self):
-        
         self.timerLock=True
         self.resetTimer()
-    
+        
+#    Останавливает таймер, возвращает разницу времени в секундах между запуском
+#    и остановкой
     def timerStop(self):
         self.timerLock=False
         timePassed=time.time() - self.startTime
         
         return timePassed
     
+#    Сбрасывает таймер, выставляя значение таймера на 0
     def resetTimer(self):
         self.startTime=time.time()
 
+
+
+
 if __name__ == '__main__':
     a = Timer()
-    a.main()
+    a.catchKeyPress()
     
 
 
